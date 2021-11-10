e050afc1eea436bbba48caaed2e50fac433da11e
SkippyAIC <91044430+SkippyAIC@users.noreply.github.com>
2021-11-10 15:56:44 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,27 @@
+"""
+MIT License
+
+Copyright (c) 2021 SkippyAIC
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 from discord.ext import commands
 import discord
 from datetime import datetime
@@ -20,7 +44,7 @@ footers = ["Feed me.", "Paws Inc., property of the SCP Foundation", "I'm not ove
 
 @bot.command(pass_context=True, name="rf")
 async def comic(ctx, *args):
-   
+    
     ## Determines Monday footer depending on whatever local timezone this script is being run in.
     activeFooters = footers
     if datetime.now().weekday() == 0:
@@ -28,7 +52,7 @@ async def comic(ctx, *args):
     else:
         activeFooters.append("I'm in a good mood... It's not Monday.")
     
-    ## If "today" is in given arguments, override with datetime.today()
+    ## If "today" is in given arguments, override with datetime.today() (or if tomorrow is in there, send THE COKE.)
     for i in args:
         if i.lower() == "today":
             ## Gets Garfield comic from current date, should be in US Eastern timezone, ideally.
@@ -38,6 +62,9 @@ async def comic(ctx, *args):
             ## Gets a random date from 1979 to 2020, a lot of limits here to ensure a proper date is picked without having to do extra processing.
             randomYear, randomMonth, randomDay = str(randint(1979, 2020)), str(randint(1, 12)), str(randint(1, 28))
             args = (randomYear, randomMonth, randomDay)
+        elif i.lower() == "tomorrow":
+            await ctx.send(r"https://cdn.discordapp.com/attachments/290667374468661249/891899409841926164/E_2j7DSVEAcccsa.png")
+            return
         
     ## Normalizes list and sends to garf.py.
     comicDate = garfDate(args)
@@ -51,8 +78,9 @@ async def comic(ctx, *args):
     
     await ctx.send(embed=garfEmbed)
     
+    
 @bot.command(pass_context=True)
-async def help(ctx): ## !gahelp
+async def help(ctx):
     
     helpEmbed = discord.Embed(title="GarfBot Help", color=0xFCAA14)
     helpEmbed.add_field(name="!garf", value="*Takes one date*\nDisplays a Garfield comic from a certain date.\n**For Example:**\n``!garf 1995 07 29``")
@@ -65,42 +93,43 @@ async def help(ctx): ## !gahelp
 @bot.command(pass_context=True, name="error")
 @commands.is_owner()
 async def triggerError(ctx, *args):
-   
     ## Intentional NameError, can only be triggered by the bot owner.
     print(test)
     
 @bot.event
 async def on_command_error(ctx, e):
     
-    ## Discord.py does not give the original Exception object, so it must be retrieved.
+    ## Discord.py does not give the original Exception object, so it must be retrieved. Also, @commands.is_owner() returns the Exception "NotOwner", which does not have a .original property.
+    ## If a user attempts to use an owner-only command, an AttributeError Exception will occur. This is normal, and can be silenced with an if statement.
     exception = e.original
     
-    ## Grabs the name of the Exception, and the reason why it occurred.
-    exceptionCause = " ".join(i for i in exception.args)
+    ## Custom exceptions like InvalidDateFormat and InvalidDate do not derive from BaseException, so the Exception name would be stored in Exception.args. Otherwise, the name would be stored in Exception.__class__.__name__.
     exceptionName = exception.__class__.__name__
+    exceptionCause = " ".join(i for i in exception.args)
     
-    if exceptionName == "InvalidDateFormat":
+    if "InvalidDateFormat" in exceptionCause:
         title = "Invalid Date Format"
-        desc = "You entered a date in an invalid format. Make sure to type YEAR-MONTH-DAY\n```!garf 1995 07 29\n!garf 1995-07-29```\nfor July 29th, 1995."
+        desc = "You entered a date in an invalid format. Make sure to type YEAR-MONTH-DAY\n**For Example:**```!garf 1995 07 29\n!garf 1995-07-29```\nfor July 29th, 1995."
         color = 0xFFFF00
-    elif exceptionName == "InvalidDate":
+    elif "InvalidDate" in exceptionCause:
         title = "Invalid Date"
         desc = "The date you entered doesn't have an associated Garfield comic!\n**If you entered today's date, the comic may not be out yet!**"
         color = 0xFFFF00
     else:
         title = "An Exception Occurred!"
-        desc = f"See the error below:\n```{exceptionName}: {exceptionCause}```\nThis is a debug message. If you want to report this message to the bot dev, go to\nhttps://github.com/skippyaic/garfbot"
+        desc = "See the error below:\n```{}: {}```\nThis is a debug message. If you want to report this message to the bot dev, go to\nhttps://github.com/skippyaic/garfbot".format(exceptionName, exceptionCause)
         color = 0xFF0000
     
     embed = discord.Embed(title=title, description=desc, color=color)
     await ctx.send(embed=embed)
     
 def garfDate(date):
-   
     ## Converts date tuple/list to string to be converted to datetime object to help discern year, month, and day.
-    dateString = "".join(i for i in date)
-    date = datetime.strptime(dateString, "%Y%m%d")
-    
+    dateString = "".join(i for i in date if i.isalpha())
+    try:
+        date = datetime.strptime(dateString, "%Y%m%d")
+    except ValueError:
+        raise Exception("InvalidDateFormat")
     zeroAdd = lambda x: "0" + str(x) if len(str(x)) == 1 else str(x)
     
     ## For year, month, and day in datetime object, add a zero to the beginning if needed, then append to a new list to be sent to the garf.py script.
@@ -109,4 +138,6 @@ def garfDate(date):
         newNumber = zeroAdd(i)
         garfCompatDate.append(newNumber)
     return garfCompatDate
+    
+
 bot.run(apikey)
